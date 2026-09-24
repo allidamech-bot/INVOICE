6e9faa5276143b7288c14b8441a45b462526b4ea
ALI M.DAAMISH <allidamech@gmail.com>
2026-09-24 12:36:21 +0300

    build: support resilient service worker install flow

--- scripts/normalize-sw-install-v262.mjs
+++ scripts/normalize-sw-install-v262.mjs
@@ -3,9 +3,13 @@ import { readFile, writeFile } from 'node:fs/promises';
 const swPath='dist/sw.js';
 let sw=await readFile(swPath,'utf8');
 
-const installRuntimeTail='await Promise.all(EXTERNAL_CORE.map(asset=>preserveExternalRuntime(cache,asset)));';
 if(!sw.includes('await self.skipWaiting();')){
-  if(!sw.includes(installRuntimeTail))throw new Error('Unable to locate the service-worker install runtime tail.');
+  const installRuntimeTails=[
+    'await Promise.allSettled(EXTERNAL_CORE.map(asset=>preserveExternalRuntime(cache,asset)));',
+    'await Promise.all(EXTERNAL_CORE.map(asset=>preserveExternalRuntime(cache,asset)));'
+  ];
+  const installRuntimeTail=installRuntimeTails.find(tail=>sw.includes(tail));
+  if(!installRuntimeTail)throw new Error('Unable to locate the service-worker install runtime tail.');
   sw=sw.replace(installRuntimeTail,`${installRuntimeTail}\n  await self.skipWaiting();`);
 }
 
